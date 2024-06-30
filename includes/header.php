bc48484b54e297850e92de90bcfe981939b91439
Melissa Lafranchise <pulchrit@gmail.com>
2024-06-30 09:13:56 -0700

    Add course resource files

---
+++ includes/header.php
@@ -0,0 +1,59 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Carousel template: Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="author" content="Udemy course: Mohamed Hassan"
+    <meta name="author" content="Course work: Melissa Lafranchise">
+    <title>Example Auth with PHP</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
+
+    
+
+    
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css
+" rel="stylesheet">
+
+
+<meta name="theme-color" content="#712cf9">
+
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">auth sys</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+     
+   
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Username
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Logout</a></li>
+           
+          </ul>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- This div is closed at the top of the footer! -->
+<div class="container marketing"> 
