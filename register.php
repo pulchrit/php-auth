0274385c067be860c029d7a8a78ae853b5708a13
Melissa Lafranchise <pulchrit@gmail.com>
2024-07-06 10:26:25 -0700

    add registration logic

--- register.php
+++ register.php
@@ -1,6 +1,43 @@
 <?php require "includes/header.php"; ?>
 
+<!-- give this file access to the db -->
+<?php require "config.php"; ?>
 
+<?php
+  if (isset($_POST['submit'])) {
+    if ($_POST['email'] == '' or $_POST['username'] == '' or $_POST['password'] == '') {
+      if ($_POST['email'] == '') {
+        echo '<p>Please enter email</p>';
+      }
+      if ($_POST['username'] == '') {
+        echo '<p>Please enter username</p>';
+      }
+      if ($_POST['password'] == '') {
+        echo '<p>Please enter password</p>';
+      }
+    } else {
+      // get values from form
+      $email = $_POST['email'];
+      $username = $_POST['username'];
+      $password = $_POST['password'];// note: this var IS password,not mypassword
+
+      // insert values into the db
+      // PDO $conn->prepare automatically paramertierizes your variables to 
+      // prevent sql injection
+      // password is a protected word, so we have to use mypassword instead
+      $insert = $conn->prepare("INSERT INTO users (email, username, mypassword)
+        VALUES (:email, :username, :mypassword)");
+
+        $insert->execute([
+          ':email' => $email, // links the value name to the value var
+          ':username'  => $username,
+          // password_hash function will hash the password before storing in the db
+          // PASSWORD_DEFAULT is the default hash mechanism
+          ':mypassword' => password_hash($password, PASSWORD_DEFAULT),
+        ]);
+    }
+  }
+?>
 
 <main class="form-signin w-50 m-auto">
   <form method="POST" action="register.php">
@@ -23,7 +60,7 @@
     </div>
 
     <button name="submit" class="w-100 btn btn-lg btn-primary" type="submit">register</button>
-    <h6 class="mt-3">Aleardy have an account?  <a href="login.php">Login</a></h6>
+    <h6 class="mt-3">Already have an account?  <a href="login.php">Login</a></h6>
 
   </form>
 </main>
